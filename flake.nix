ebac037a8923af3b13a45353966f6b9bcb2bdc69
nullishamy <git@amyerskine.me>
2024-08-31 19:36:18 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs =
+          let
+          custom-kubernetes-helm = with pkgs; wrapHelm kubernetes-helm {
+            plugins = with pkgs.kubernetes-helmPlugins; [
+              helm-secrets
+              helm-diff
+              helm-s3
+              helm-git
+            ];
+          };
+          custom-helmfile = pkgs.helmfile-wrapped.override {
+            inherit (custom-kubernetes-helm) pluginsDir;
+          }; 
+          in with pkgs; [
+            kubectl
+            custom-helmfile
+            custom-kubernetes-helm
+          ];
+          env = {
+            KUBECONFIG = "/home/amy/code/kube.config/kubeconfig";
+          };
+        };
+      }
+    );
+}
